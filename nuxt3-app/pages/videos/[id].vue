da889a91aba967d77ad47a2e92d73bad7239bbd7
Tiago Oliveira de Farias <tiago.farias.poa@gmail.com>
2024-03-04 09:59:38 -0300

    Add - router with parameter

---
+++ nuxt3-app/pages/videos/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Voce está acessando o video numero {{ $route.params.id }}
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
